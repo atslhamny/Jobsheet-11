16b779f606d26c57d13efada5a06c29e6aa695a3
atslhamny <atsilahamanyph@gmail.com>
2023-11-09 08:47:13 +0700

    membuat segitiga bintang

---
+++ Triangle04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Triangle04
+ */
+public class Triangle04 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan Nilai N = ");
+        int N = input.nextInt();
+        int i = 0;
+        while (i<=N) {
+            int j = 0;
+            while (j<i) {
+                System.out.print("*");
+                j++;
+            }
+            //System.out.println();
+            i++;
+        }
+    }
+}
