e4b8712b9eee6679b0cd6b13c30156543d51a7a0
atslhamny <atsilahamanyph@gmail.com>
2023-11-09 09:57:56 +0700

    membuat bintang segitiga menyamping kiri

---
+++ percobaan2Absen04.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class percobaan2Absen04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N : ");
+        int N = sc.nextInt();
+
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= N - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
