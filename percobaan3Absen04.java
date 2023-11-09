e33ade5949c07864810432772354d62345ebe699
atslhamny <atsilahamanyph@gmail.com>
2023-11-09 10:09:53 +0700

    membuat kotak angka

---
+++ percobaan3Absen04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class percobaan3Absen04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N : ");
+        int N = sc.nextInt();
+
+        if (N >= 3) {
+
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (i == 1 || i == N || j == 1 || j == N) {
+                        System.out.print(" " + N);
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+        } 
+    }
+}
