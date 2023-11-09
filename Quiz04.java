be55cc127f2b8c900e2daa092d071fb68a0c3a82
atslhamny <atsilahamanyph@gmail.com>
2023-11-09 09:18:06 +0700

    membuat kuis tebak angka

--- Quiz04.java
+++ Quiz04.java
@@ -1,3 +1,33 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Quiz04 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        char menu ='y';
+        do {
+            int angka = random.nextInt(10) + 1;
+            boolean sukses = false;
+            do {
+                System.out.print("Tebak Angka (1-10): ");
+                int jawaban = input.nextInt();
+                input.nextLine();
+                if (jawaban < angka) {
+                    System.out.println("Tebakan Anda terlalu kecil!");
+                } else if (jawaban > angka) {
+                    System.out.println("Tebakan Anda terlalu besar!");
+                } else {
+                    sukses = true;
+                    System.out.println("Selamat, tebakan Anda benar!");
+                }
+            } while (!sukses);
+            System.out.println("Apakah Anda Ingin Mengulangi Permainan ? (Y/y) = ");
+            menu = input.nextLine().charAt(0);
+        } while (menu=='y' || menu=='Y');
+        System.out.println("Terima kasih telah bermain!");
+        input.close();
+    }
 }
