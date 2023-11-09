6595f02180416c4beb51f4a6a9466260f56407ea
atslhamny <atsilahamanyph@gmail.com>
2023-11-09 08:29:43 +0700

    membuat perulangan bintang

---
+++ Star04.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Star04
+ */
+public class Star04 {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan Nilai N = ");
+        int N = input.nextInt();
+
+        for (int i = 1; i <= N; i--) {
+            System.out.print("*");
+        }
+    }
+}
